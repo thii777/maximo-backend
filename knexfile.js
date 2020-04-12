00e9b33ec0c7008944076fb1ceb7051e9eb71036
Thiago Carvalho <thiagocarvalho.ads@gmail.com>
2020-04-12 19:19:48 -0300

    wip: config heroku pg

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,24 @@
 require('dotenv').config()
-const { Client } = require('pg')
+// const { Client } = require('pg')
 
-const connectionString = process.env.DATABASE_URL
+// const connectionString = process.env.DATABASE_URL
 
-const database_url = new Client({
-  connectionString: connectionString
-})
+// const database_url = new Client({
+//   connectionString: connectionString
+// })
 
 module.exports = {
   production: {
     client: 'postgres',
-    connection: {
-      database_url: database_url
-      // host: process.env.DATABASE_HOST,
-      // database: process.env.DATABASE_NAME,
-      // port: process.env.DATABASE_PORT,
-      // user: process.env.DATABASE_USER,
-      // password: process.env.DATABASE_PASSWORD
-    },
-
+    connection: process.env.DATABASE_URL,
+    // {
+    //   database_url: database_url,
+    //   host: process.env.DATABASE_HOST,
+    //   database: process.env.DATABASE_NAME,
+    //   port: process.env.DATABASE_PORT,
+    //   user: process.env.DATABASE_USER,
+    //   password: process.env.DATABASE_PASSWORD
+    // },
     migrations: {
       directory: './src/database/migrations'
     },
